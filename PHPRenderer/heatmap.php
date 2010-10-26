66a64c69becea5e9d7f2b33cc0deebb37cd8d4cb
David Walsh <davidwalsh83@gmail.com>
2010-10-25 22:02:40 -0500

    Updating version.

--- PHPRenderer/heatmap.php
+++ PHPRenderer/heatmap.php
@@ -1,53 +1,53 @@
 <?php
-/* load  */
-if(isset($_GET['load'])) {
+	/* load  */
+	if(isset($_GET['load'])) {
 	
-	/* vars */
-	$spots = array();
+		/* vars */
+		$spots = array();
 	
-	/* connect to the db */
-	$connection = mysql_connect('localhost','dbuser','dbpass');
-	mysql_select_db('dbname',$connection);
+		/* connect to the db */
+		$connection = mysql_connect('localhost','dbuser','dbpass');
+		mysql_select_db('dbname',$connection);
 	
-	/* get spots */
-	$query = 'SELECT * FROM example_heatmap WHERE zone = \''.mysql_escape_string($_GET['zone']).'\' LIMIT 2000';
-	$result = mysql_query($query,$connection);
-	while($record = mysql_fetch_assoc($result)) {
-		$spots[] = $record;
+		/* get spots */
+		$query = 'SELECT * FROM example_heatmap WHERE zone = \''.mysql_escape_string($_GET['zone']).'\' LIMIT 2000';
+		$result = mysql_query($query,$connection);
+		while($record = mysql_fetch_assoc($result)) {
+			$spots[] = $record;
+		}
+	
+		/* close db connection */
+		mysql_close($connection);
+	
+		/* return result */
+		$json = json_encode($spots);
+		echo $json;
+		die();
 	}
+	/* save */
+	elseif(isset($_GET['save']) && isset($_GET['data']) && count($_GET['data'])) {
 	
-	/* close db connection */
-	mysql_close($connection);
+		/* vars */
+		$query = 'INSERT INTO example_heatmap (zone,x,y,date_clicked) VALUES ';
+		$queryRecords = array();
+		$records = 0;
 	
-	/* return result */
-	$json = json_encode($spots);
-	echo $json;
-	die();
-}
-/* save */
-elseif(isset($_GET['save']) && isset($_GET['data']) && count($_GET['data'])) {
+		/* connect to the db */
+		$connection = mysql_connect('localhost','dbuser','dbpass');
+		mysql_select_db('dbname',$connection);
 	
-	/* vars */
-	$query = 'INSERT INTO example_heatmap (zone,x,y,date_clicked) VALUES ';
-	$queryRecords = array();
-	$records = 0;
+		/* save! */
+		foreach($_GET['data'] as $data) {
+			$queryRecords[] =  '(\''.mysql_escape_string($_GET['zone']).'\','.mysql_escape_string($data['x']).','.mysql_escape_string($data['y']).',NOW())';
+			$records++;
+		}
 	
-	/* connect to the db */
-	$connection = mysql_connect('localhost','dbuser','dbpass');
-	mysql_select_db('dbname',$connection);
+		/* execute query, close */
+		$query.= implode(',',$queryRecords);
+		mysql_query($query,$connection);
+		mysql_close($connection);
 	
-	/* save! */
-	foreach($_GET['data'] as $data) {
-		$queryRecords[] =  '(\''.mysql_escape_string($_GET['zone']).'\','.mysql_escape_string($data['x']).','.mysql_escape_string($data['y']).',NOW())';
-		$records++;
+		/* return result */
+		die(count($records));
 	}
-	
-	/* execute query, close */
-	$query.= implode(',',$queryRecords);
-	mysql_query($query,$connection);
-	mysql_close($connection);
-	
-	/* return result */
-	die(count($records));
-}
 ?>
